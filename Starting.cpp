4c791f061b4045ff2fea8d99691d88c5e33bb87a
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-09-20 19:24:51 +0530

    first commit

---
+++ Starting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    // Calculate the sum
+    sum = num1 + num2;
+
+    // Output the sum
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
