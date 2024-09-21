6d75ba5e7a45f0e643bbb630908b02dac42efea1
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-09-21 20:52:10 +0530

    logical-operators-binary-unary-operators

--- Starting.cpp
+++ Starting.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
 
+// this is the starting point of execution
+
 int main()
 {
-    int num1, num2, sum;
+    int num1, sum;
+    float num2;
 
     // Input two numbers
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
+    // cout << "Enter two numbers: ";
+    // cin >> num1 >> num2;
 
     // Calculate the sum
-    sum = num1 + num2;
+    // sum = num1 + num2;
 
     // Output the sum
-    cout << "The sum is: " << sum << endl;
-
+    // cout << "The sum is: " << sum << endl;
+    // cout << "The sum is:" << (num1 + num2) << endl;
+    // cout << "The Multiplication is:" << (num1 * num2) << endl;
+    // cout << "The Remainder is:" << (num1 % num2) << endl;
+    // cout << "The Division is:" << (num1 / num2) << endl;
+    // cout << "The Division is:" << (5 / (float)2) << endl;
+    cout << (0 - 1) << endl;
+    cout << (1 - 0) << endl;
     return 0;
 }
