f1666fa11412f978df28627908c891ae5474ef53
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-10-12 20:46:54 +0530

    2-patterns

---
+++ SquarePatternChar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        char ch = 'A';
+        for (int j = 0; j < num; j++)
+        {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
