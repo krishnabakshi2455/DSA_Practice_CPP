a0d20892b72292d4839dc05728f7d1f77009438c
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-10-15 21:06:47 +0530

    2 patterns

---
+++ SquarePatternNumber2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int num = 3;
+    int num2 = 1;
+    for (int row = 0; row < num; row++)
+    {
+        for (int col = 0; col < num; col++){
+            cout<< num2;
+            num2 ++;
+        }
+      
+        
+        cout<<endl;
+        
+    }
+    
+
+
+    return 0;
+}
